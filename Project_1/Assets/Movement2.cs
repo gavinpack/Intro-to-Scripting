0531ef9bcf429d11724532e473d72c9ce7739ed8
Gavin Pack <gavinpack@gmail.com>
2019-09-19 15:47:42 -0600

    Character Controller Re-Write + Double Jump

---
+++ Project_1/Assets/Movement2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement2 : MonoBehaviour
+{
+    private CharacterController controller;
+    public float moveSpeed = 10f, gravity = 9.81f, jumpSpeed = 30f;
+    private Vector3 position;
+    private int jumpCount;
+    public int jumpCountMax = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = moveSpeed*Input.GetAxis("Horizontal");
+        position.y -= gravity;
+
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+        }
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+
+        controller.Move(position*Time.deltaTime);
+    }
+}
