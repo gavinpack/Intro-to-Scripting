ba0f39b4dd7f05b71c5a27740e32c3e1668a674e
Gavin Pack <gavinpack@gmail.com>
2019-10-01 15:49:09 -0600

    Int Data & UI Text Controller

---
+++ Project_1/Assets/IntData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value = 1;
+
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+}
---
+++ Project_1/Assets/UITextController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UITextController : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    private Text textLabel;
+
+    public void UpdateText(IntData data)
+    {
+        textLabel.text = data.value.ToString()
+    }
+    void Start()
+    {
+        textLabel = GetComponent<Text>();
+        startEvent.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
