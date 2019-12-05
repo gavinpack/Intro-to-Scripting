1556e2afbfb87dbbfdc895c2117a7545f218a7f1
Gavin Pack <gavinpack@gmail.com>
2019-12-05 15:57:38 -0700

    Game Action and Instancer

---
+++ Project_1/Assets/Scripts/GameActionRaiser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameActionRaiser : MonoBehaviour
+{
+    public float holdTime = 1f;
+    public WaitForSeconds waitForSeconds;
+    private bool _canRun = true;
+    public GameAction gameActionObj;
+
+    private void Awake()
+    {
+        waitForSeconds = new WaitForSeconds(holdTime);
+    }
+    private IEnumerator OnMouseDown()
+    {
+        _canRun = true;
+        while (_canRun)
+        {
+            gameActionObj.Raise();
+            yield return waitForSeconds;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        _canRun = false;
+    }
+}
